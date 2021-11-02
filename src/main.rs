e705c85a6958c73a7c4e71372501e2d5a6ef2c83
Green-Avocado <greenavocado@protonmail.com>
2021-11-02 01:17:24 -0700

    working channels

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,117 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::{
+    async_trait,
+    model::{
+        channel::Message,
+        prelude::Ready,
+    },
+    client::{
+        Client,
+        Context,
+        EventHandler,
+    },
+    framework::standard::{
+        CommandResult,
+        StandardFramework,
+        macros::{
+            command,
+            group,
+        },
+    },
+};
+
+use tokio::sync::mpsc::{
+    Receiver,
+    Sender,
+    channel,
+};
+
+use typemap_rev::TypeMapKey;
+
+use std::{
+    sync::Arc,
+    env,
+};
+
+struct WriteQueueSender;
+
+impl TypeMapKey for WriteQueueSender {
+    type Value = Arc<Sender<String>>;
+}
+
+#[group]
+#[commands(
+    balance,
+    pay,
+    charge,
+    )
+]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, rx): (Sender<String>, Receiver<String>) = channel(16);
+
+    tokio::spawn(async move {
+        let mut rxm = rx;
+        loop {
+            let received = rxm.recv().await.unwrap();
+            println!("Got: {}", received);
+        }
+    });
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix(""))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<WriteQueueSender>(Arc::new(tx));
+    }
+
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn balance(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Balance: ").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn pay(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pay").await?;
+
+    let tx = ctx.data.read().await.get::<WriteQueueSender>().unwrap().clone();
+    tx.send("Payment".to_string()).await.unwrap();
+
+    Ok(())
+}
+
+#[command]
+async fn charge(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Charge").await?;
+
+    let tx = ctx.data.read().await.get::<WriteQueueSender>().unwrap().clone();
+    tx.send("Charge".to_string()).await.unwrap();
+
+    Ok(())
 }
